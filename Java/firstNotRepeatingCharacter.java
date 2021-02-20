9ecc869a40a0668e785abd9b6c1d99de61ef2278
Valnati <workforthefuture@gmail.com>
2021-02-19 17:23:50 -0800

    Create firstNotRepeatingCharacter.java

---
+++ Java/firstNotRepeatingCharacter.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class firstNotRepeatingCharacter {
+
+	/***
+	 * Given a string s consisting of small English letters, 
+	 * find and return the first instance of a non-repeating 
+	 * character in it. If there is no such character, return '_'.
+	 * 
+	 * Original answer used Hashmap
+   * also consider simple array, so position is saved inherently
+	 * 
+	 * @param s
+	 * @return
+	 */
+	
+    public static void main (String[] args) {
+        String a = "I am a string with many duplicates";
+        System.out.println(firstNotRepeatingCharacter(a));
+    }
+
+	public static char firstNotRepeatingCharacter(String s) {
+	    if (s.length()==1) return s.charAt(0);
+	    if (s.length() <= 0) return '_';
+	    Map<Character, Integer> knownChars = new HashMap<>();
+	    for (int i = 0; i < s.length(); i++) {
+	        if (knownChars.containsKey(s.charAt(i))) {
+	            knownChars.merge(s.charAt(i), 1, Integer::sum);
+	        }
+	        else knownChars.put(s.charAt(i), 1);
+	    }
+	    for (int i= 0; i < s.length(); i++) {
+	        if (knownChars.get(s.charAt(i)) == 1) return s.charAt(i);
+	    }
+	    return '_';
+	}
+}
