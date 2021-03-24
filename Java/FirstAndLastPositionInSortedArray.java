1b1c4b556b70c13bf80137873403dfd169dac442
Valnati <workforthefuture@gmail.com>
2021-03-24 16:14:54 -0700

    Update FirstAndLastPositionInSortedArray.java

--- Java/FirstAndLastPositionInSortedArray.java
+++ Java/FirstAndLastPositionInSortedArray.java
@@ -1,4 +1,15 @@
-package Tests;
+/**********************
+*Given a sorted array, return the range of the specified
+*target value. Must be done within O(logn).
+*
+*A simple binary search extended to cover the 
+*full breadth of a possible value, instead of 
+*hunting for the first copy of it.
+*
+*Time is O(logn), as per usual binary search.
+*Space is constant, with an int array length 2
+*and 6 int variables made.
+*********************/
 
 public class FirstAndLastPositionInSortedArray {
 
@@ -40,7 +51,6 @@ public class FirstAndLastPositionInSortedArray {
 			midpoint = start + (end-start)/2;
 			if (target >= nums[midpoint]) start = midpoint + 1;
 			else end = midpoint - 1;
-			System.out.println("Start: " + start + "    MID: " + midpoint + "    END: " + end );
 			if (nums[midpoint] == target) index = midpoint;
 		}
 		return index;
@@ -49,42 +59,10 @@ public class FirstAndLastPositionInSortedArray {
 	public static void main(String[] args) {
 		int[] nums = {6,6,6,6,6,6, 6,6,6,6};
 		int[] sol = solution(nums, 6); 
-		System.out.println(sol[0] + " " + sol[1]);
+		System.out.println(sol[0] + " " + sol[1]); //returns [0, 9]
 		
 		int[] nums2 = {5,7,7,8,8,10};
 		sol = solution(nums2, 8); 
-		System.out.println(sol[0] + " " + sol[1]);
-	}
-	
-	
-	
-	//public static int coolerSolution() {
-		/*
-		class Solution {
-		    int findLeft(int nums[],int target){
-		        int l=0,r=nums.length-1;
-		        while(l<=r){
-		            int m=l+(r-l)/2;
-		            if(nums[m]>=target) r=m-1;
-		            else l=m+1;
-		        }
-		        return r+1<nums.length && nums[r+1]==target ? r+1 : -1;
-		    }
-		     int findRight(int nums[],int target){
-		        int l=0,r=nums.length-1;
-		        while(l<=r){
-		            int m=l+(r-l)/2;
-		            if(nums[m]<=target) l=m+1;
-		            else r=m-1;
-		        }
-		        return l-1>=0 && nums[l-1]==target ? l-1 : -1;
-		    }
-		    public int[] searchRange(int[] nums, int target) {
-		        int left=findLeft(nums,target);
-		        int right=findRight(nums,target);
-		        return new int[]{left,right};
-		    }
-		}
-		****/
+		System.out.println(sol[0] + " " + sol[1]); //returns [3,4]
 	}
 }
